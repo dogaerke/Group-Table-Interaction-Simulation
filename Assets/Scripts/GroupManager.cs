e184941e2206a2a133d5379bf0dbdd57d8a01ed6
Doğa Melis ERKE <dogaerke2001@gmail.com>
2024-11-02 16:23:39 +0300

    Added: Queue in line, shifting, moving to exit

--- Assets/Scripts/GroupManager.cs
+++ Assets/Scripts/GroupManager.cs
@@ -1,27 +1,69 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class GroupManager : MonoBehaviour
 {
+    private int _randomNum;
+    
     [SerializeField] private Person instancePrefab;
-    [SerializeField] private float spawnRadius = 50f;
-    [SerializeField] private float minDistance = 5f;
+    [SerializeField] private float spawnRadius = 6f;
+    [SerializeField] private float minDistance = 1.2f;
+
+    public List<Transform> inLinePointList;
+    public Transform exitPoint;
+    public Transform tablePoint;
+    
+    public List<Person> waitingPersonList = new List<Person>();
+    public List<Person> walkingPersonList = new List<Person>();
+    public List<Person> inLinePersonList = new List<Person>();
+    public List<Person> exitingPersonList = new List<Person>();
+    public List<Person> determinedWalkingPersonList = new List<Person>();
     
     public int instanceCount = 30;
-    public Transform destinationPoint;
-    
+    public bool isInCoroutine;
+
     public static GroupManager Instance { get; private set; }
 
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+
+        else
+            Instance = this;
+    }
+    
     private void Start()
     {
         SpawnInstances();
 
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && waitingPersonList.Count > 0 && 
+            inLinePersonList.Count + walkingPersonList.Count + determinedWalkingPersonList.Count < inLinePointList.Count)
+        {
+            _randomNum = Random.Range(0, waitingPersonList.Count);  //Choose random person
+            
+            waitingPersonList[_randomNum].MoveInstanceToLine();
+            
+        }
+
+    }
+
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
     private void SpawnInstances()
     {
         var createdInstancesCount = 0;
@@ -46,16 +88,68 @@ public class GroupManager : MonoBehaviour
             if (canPlace)
             {
                 var instance = Instantiate(instancePrefab, randomPos, Quaternion.identity, transform);
-                instance.SetDestination(destinationPoint);
-
+                waitingPersonList.Add(instance);
+                
                 createdInstancesCount++;
             }
             attempts++;
-            
+
         }
         
         if (createdInstancesCount < instanceCount)
             Debug.LogWarning("There is not enough space in the circle!");
+        
+    }
+    
+    public void AddInLine(Person instance)
+    {
+        instance.OnReach -= AddInLine;
+        
+        inLinePersonList.Add(instance);
+        
+        if(determinedWalkingPersonList.Count > 0)
+            determinedWalkingPersonList.Remove(instance);
+        
+        if (!isInCoroutine)
+            inLinePersonList[0].OnInteract += InteractWithTable;
+
+        
+    }
+    
+    public void ShiftCustomersInLine()
+    {
+        if(inLinePersonList.Count == 0) return;
+        
+        var i = 0;
+        foreach (var c in inLinePersonList)
+        {
+            c.SetDestination(inLinePointList[i++].transform);
+        }
+        
+        if (!isInCoroutine)
+            inLinePersonList[0].OnInteract += InteractWithTable;
+
+        
+    }
+    
+    private void InteractWithTable(Person instance)
+    {
+        instance.OnReach -= InteractWithTable;
+        
+        StartCoroutine(WaitForSeconds(5f, () =>
+        {
+            inLinePersonList[0].MoveInstanceToExit();
+
+        }));
+        
+    }
+    
+    private IEnumerator WaitForSeconds(float t, Action onWaitEnd)
+    {
+        isInCoroutine = true;
+        yield return new WaitForSeconds(t);
+        isInCoroutine = false;
+        onWaitEnd?.Invoke();
     }
     
 }
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -8,29 +8,120 @@ using Random = UnityEngine.Random;
 
 public class Person : MonoBehaviour
 {
+    public event Action<Person> OnReach;
+    public event Action<Person> OnDestination;
+    public event Action<Person> OnInteract;
+
     [SerializeField] private NavMeshAgent agent;
-    private Transform target;
-    private bool _canMove;
-    private float _speed;
+    public int speed;
     
+    private Transform _target;
+    private bool _isDestDetermined;
 
     private void Start()
     {
         DetermineRandomSpeed();
+        agent.isStopped = true;
+    }
+
+    private void Update()
+    {
+        IsPersonStopped();
+        DetermineDestinationPoint();
     }
     
+    private void IsPersonStopped()
+    {
+        if(!_target)
+            return;
+        
+        var myPos = transform.position;
+        myPos.y = 0f;                           
+        var targetPos = _target.position;
+        targetPos.y = 0f;
+        if (Vector3.Distance(myPos, targetPos) < .2f)
+        {
+            agent.isStopped = true;
+            _target = null;
+            OnReach?.Invoke(this);
+            
+            if(GroupManager.Instance.inLinePersonList.Count > 0 && this == GroupManager.Instance.inLinePersonList[0])
+                OnInteract?.Invoke(this);
+        }
+        
+    }
+    
+
+    private void DetermineDestinationPoint() //X=2 Point
+    {
+        if (GroupManager.Instance.exitingPersonList.Contains(this)) return;
+        
+        if (Mathf.Abs(transform.position.x - GroupManager.Instance.inLinePointList
+                [GroupManager.Instance.inLinePersonList.Count + GroupManager.Instance.determinedWalkingPersonList.Count].position.x) < .2f && !_isDestDetermined)
+        {
+            _isDestDetermined = true;
+            OnDestination?.Invoke(this);
+            Debug.Log("OnDestination invoked " + " transform.position.x is " + transform.position.x 
+                      + " inLinePersonList.Count " + GroupManager.Instance.inLinePersonList.Count);
+            
+        }
+    }
+    public void MoveInstanceToLine()
+    {
+        if (GroupManager.Instance.inLinePersonList.Count + GroupManager.Instance.walkingPersonList.Count + 
+         GroupManager.Instance.determinedWalkingPersonList.Count >= GroupManager.Instance.inLinePointList.Count) return;
+        
+        if(GroupManager.Instance.waitingPersonList.Count > 0)
+            GroupManager.Instance.waitingPersonList.Remove(this);
+        
+        SetDestination(GroupManager.Instance.inLinePointList[GroupManager.Instance.inLinePersonList.Count + GroupManager.Instance.determinedWalkingPersonList.Count]);
+                                                                                                                      
+        GroupManager.Instance.walkingPersonList.Add(this);
+        
+        OnDestination += SetDestinationPoint;
+
+    }
+    
+    private void SetDestinationPoint(Person instance)
+    {
+        OnDestination -= SetDestinationPoint;
+
+        GroupManager.Instance.determinedWalkingPersonList.Add(this);
+        
+        if(GroupManager.Instance.walkingPersonList.Count > 0)
+            GroupManager.Instance.walkingPersonList.Remove(this);
+        
+        SetDestination(GroupManager.Instance.inLinePointList
+            [GroupManager.Instance.inLinePersonList.Count + GroupManager.Instance.determinedWalkingPersonList.IndexOf(this)]);
+    
+        OnReach += GroupManager.Instance.AddInLine;
+
+    }
+    
+    public void MoveInstanceToExit()
+    {
+        SetDestination(GroupManager.Instance.exitPoint);
+        
+        if(GroupManager.Instance.inLinePersonList.Count > 0)
+            GroupManager.Instance.inLinePersonList.Remove(this);
+        
+        GroupManager.Instance.exitingPersonList.Add(this);
+
+        GroupManager.Instance.ShiftCustomersInLine();
+
+    }
     
     public void SetDestination(Transform target)
     {
-        this.target = target;
+        _target = target;
         agent.SetDestination(target.position);
-        _canMove = true;
         agent.isStopped = false;
+        
     }
 
     private void DetermineRandomSpeed()
     {
-        var x = Random.Range(2, 11);
-        agent.speed = x;
+        speed = Random.Range(2, 11);
+        agent.speed = speed;
     }
 }
